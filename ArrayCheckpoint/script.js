ebfc19fbf55cb0bba6b91ede98e953f2572d7c19
FosterCareAwareness <51935764+FosterCareAwareness@users.noreply.github.com>
2020-03-30 11:11:54 -0400

    Array Checkpoint

---
+++ ArrayCheckpoint/script.js
@@ -0,0 +1,6 @@
+var drinks = [' Water, Coffee, Tea ']
+var foods = [' Apple, sandwhich, Chips ']
+var meals = [' Water, Coffee, Tea, Apple, Sandwhich, Chips ']
+var reverse = meals.reverse();
+
+console.log(reverse);
